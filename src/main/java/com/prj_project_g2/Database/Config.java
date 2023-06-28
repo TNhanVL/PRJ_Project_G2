26e43cbe0614cc0c8fa583ac8891c01b5889aae3
TTNhan <nhan12341184@gmail.com>
2023-06-28 22:18:24 +0700

    add config file

---
+++ src/main/java/com/prj_project_g2/Database/Config.java
@@ -0,0 +1,10 @@
+package com.prj_project_g2.Database;
+
+public interface Config {
+
+    String SERVER = "VICTUS-16";
+    String USER = "sa";
+    String PASSWORD = "a";
+    String DATABASE_NAME = "DB_PRJ_Project_G2";
+    int PORT = 1433;
+}
